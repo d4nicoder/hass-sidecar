9ebda7c08d4826e5df51e03381d2a14679fabfd9
Daniel Cañada García <danitetus@gmail.com>
2020-09-21 00:50:13 +0200

    Commented API.ts

--- src/API.ts
+++ src/API.ts
@@ -1,50 +1,69 @@
 import websocketConnection from "./websocketConnection"
 import { IState, IStateCallback } from "./interfaces/IState"
-import moment from 'moment'
 import path from 'path';
 import chokidar from 'chokidar'
 import { Automation } from "./interfaces/Automation";
 
-import MQTT from './mqtt'
 import Logger from './lib/Logger';
 
+/**
+ * Class to manage all interactions with the backend
+ *
+ * @class API
+ */
 class API {
 
   private _automations: Map<string, Automation> = new Map()
   private _connection: websocketConnection
   private _states: Map<string, IState> = new Map()
   private _stateListeners: Map<string, {id: number, callback:IStateCallback}[]> = new Map()
-  private _nextSunset: Date = new Date()
-  private _nextSunrise: Date = new Date()
 
   private static _instance: API
+
+  /**
+   * Creates an instance of API.
+   *
+   * @memberof API
+   */
   public constructor() {
     const host: string = process.env.HA_HOST || ''
     const token: string = process.env.HA_TOKEN || ''
 
+    // Initiate the websocket connection
     this._connection = new websocketConnection(host, token)
 
+    // When the connection is ready, sync all states
     this._connection.addEventListener('ready', () => {
       Logger.info('Connection ready')
 
       this._syncStates()
         .then(() => {
           Logger.info('States synced')
+
+          // Loading all automations when states are synced
           this._bootstrap()
         })
         .catch((error) => {
           Logger.error(error)
         })
+
+      // Main function to watch states changes
       this._onStateChange()
     })
 
+    // If connection closes, unload all automations
     this._connection.onClose(() => {
       this._unload()
     })
-
-    const mqtt = MQTT.getInstance()
   }
 
+  /**
+   * Singleton
+   *
+   * @static
+   * @returns {API} Instance
+   * @memberof API
+   */
   public static getInstance(): API {
     if (!API._instance) {
       API._instance = new API()
@@ -53,12 +72,22 @@ class API {
     return API._instance
   }
 
+  /**
+   * Subscribe to state changes on entity
+   *
+   * @param {string} entityId              entity_id to watch for changes
+   * @param {IStateCallback} callback      callback function
+   * @returns                listenerInfo  Object with the listener information
+   * @memberof API
+   */
   public onState(entityId: string, callback: IStateCallback) {
     const id = Math.floor(Math.random() * 10000) + new Date().getTime()
     const listenerInfo = {
       id,
       entityId
     }
+
+    // Store this callback to the _stateListeners map
     if (!this._stateListeners.has(entityId)) {
       this._stateListeners.set(entityId, [{id, callback}])
     } else {
@@ -74,6 +103,13 @@ class API {
     return listenerInfo
   }
 
+  /**
+   * Unsubscribe from state change.
+   *
+   * @param {string} entityId   entity to unsubscribe
+   * @param {number} id         id subscription
+   * @memberof API
+   */
   public clearOnState(entityId: string, id: number) {
     if (this._stateListeners.has(entityId)) {
       let listeners = this._stateListeners.get(entityId)
@@ -89,8 +125,11 @@ class API {
   }
 
   /**
-   * Get states
-   * Get all entities states and stores in a Map
+   * Get current state of one entity
+   *
+   * @param {string} entityId      entity_id to get state
+   * @returns {Promise<IState>}    Promise qith the state
+   * @memberof API
    */
   public async getState(entityId: string): Promise<IState> {
     const state = this._states.get(entityId)
@@ -100,15 +139,28 @@ class API {
     return state
   }
 
+  /**
+   * Call a Home Assistant service
+   *
+   * @param {string} domain              Domain, for example: light
+   * @param {string} service             Service, for example: turn_on
+   * @param {(string | null)} entityId   Entity id
+   * @param {*} data                     Attributes (optional)
+   * @returns {Promise<any>}             Promise with the result
+   * @memberof API
+   */
   public callService(domain: string, service: string, entityId: string | null, data: any): Promise<any> {
     return new Promise((resolve, reject) => {
       let options: any = {
         entity_id: entityId
       }
+
+      // Merging options
       if (data && data !== {}) {
         options = {...options, ...data}
       }
 
+      // Some services don't require an entity_id
       if (!entityId) {
         delete options.entity_id
       }
@@ -129,7 +181,14 @@ class API {
     })
   }
 
-  private async _syncStates() {
+  /**
+   * Get all states and stores their values in the map
+   *
+   * @private
+   * @returns {Promise<void>}      Promise
+   * @memberof API
+   */
+  private async _syncStates(): Promise<void> {
     return new Promise((resolve, reject) => {
       this._connection.getStates()
         .catch(reject)
@@ -152,7 +211,16 @@ class API {
     })
   }
 
+  /**
+   * Initiates subscription to all states changes.
+   * Logs any change to the console, stores the new state in the states map
+   * and call all callback functions from listeners.
+   *
+   * @private
+   * @memberof API
+   */
   private _onStateChange() {
+    // Subscribing to Home Assistant state_change event
     this._connection.subscribeEvent('state_changed', (message) => {
       const newState: IState = message.data.new_state
       if (!newState) {
@@ -160,11 +228,6 @@ class API {
       }
       Logger.debug(`New state of ${newState.attributes.friendly_name} (${newState.entity_id}): ${newState.state}`)
       this._states.set(newState.entity_id, newState)
-
-      if (!message.data.old_state) {
-        // Force old_state exists
-        message.data.old_state = message.data.new_state
-      }
 
       if (this._stateListeners.has(newState.entity_id)) {
         const listeners = this._stateListeners.get(newState.entity_id)
@@ -184,6 +247,15 @@ class API {
       })
   }
 
+  /**
+   * Load all automations and watch for changes
+   * When automation file changes, it will be unloaded
+   * and loaded on the fly
+   *
+   * @private
+   * @returns {Promise<void>}
+   * @memberof API
+   */
   private async _bootstrap (): Promise<void> {
     const automationsDir: string = path.resolve(path.join(__dirname, 'automations'))
     // Watch for changes
@@ -199,6 +271,16 @@ class API {
     })
   }
 
+  /**
+   * Handle modified automation file
+   * If automation has previously loaded, unloads it an reloads.
+   * It the automation is new, loads it
+   *
+   * @private
+   * @param {string} ev            Event fired (change or add)
+   * @param {string} filename      File path that has changed
+   * @memberof API
+   */
   private _modifiedFile(ev: string, filename: string) {
     if (!/\.ts$/.test(filename) || /\/\.?lib\//.test(filename)) {
       return
@@ -222,6 +304,12 @@ class API {
     }
   }
 
+  /**
+   * Unloads automation class
+   *
+   * @private
+   * @memberof API
+   */
   private _unload() {
     Array.from(this._automations).forEach((automation) => {
       Logger.debug(`Unloading ${automation[0]}`)
